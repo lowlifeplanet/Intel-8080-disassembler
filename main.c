4835b9e2be3491689bf06413d16471db38419f95
tornado711 <tornado71154@gmail.com>
2019-02-14 11:44:42 -0500

    Adds reading buffer input, and sets up all buffer and memory to read disassembled file. Adds skeleton code while loop

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<errno.h>
 int main(int argc, char* argv[]) {
-    char* buff[16];
-   int file;
-    if((file = open(argv[1], O_CREAT)) <= 0){
-        printf("Error!");
+   
+   FILE *file;
+    if((file = fopen(argv[1], "r")) <= 0){
+	int retcode = errno;
+        perror("File error: ");
+	exit(retcode);
     }else{
-        printf("success!");
+        printf("Successfuly opened file: %s \n", argv[1] );
     }
 
-    read(file, buff, 16);
-    printf("done!");
-
-    for(int i = 0; i < 100; i++){
-        printf("%x", buff[i]);
+    fseek(file, 0, SEEK_END);
+    int size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    unsigned char buff[size];
+    printf("Buffisze is: %d\n Reading %s into buffer...\n", size, argv[1]);
+    size_t length;
+    if((length = fread(buff, 1, size, file)) <= 0){
+      perror("Buffer input error: ");
+      int retcode = errno;
+      exit(errno);
+    }else{
+      printf("Buffer read into Memory successfully!\n Amount of bytes read: %d", length);
+    }
+    int exit = 0;
+    while(exit == 0){
+	
     }
 
     return 0;
